a0991ef2308baa018725fa497fb74e9fd4e71025
Mikhail Varushichev <mikeloangel@gmail.com>
2023-01-07 11:53:36 +0100

    fix: JOI signup name is required now

--- request_validation_schemas/auth-signup.js
+++ request_validation_schemas/auth-signup.js
@@ -2,7 +2,7 @@ const { Joi } = require('celebrate');
 
 const authSignupSchema = Joi.object().keys({
   email: Joi.string().required().email(),
-  name: Joi.string().min(2).max(30),
+  name: Joi.string().min(2).max(30).required(),
   password: Joi.string().required(),
 });
 
